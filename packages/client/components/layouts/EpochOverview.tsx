b4cefc0d892dbc4777066f82ccee73dc765ecaaa
Josep Chetrit <jchetrit1999@gmail.com>
2022-12-06 13:43:03 +0100

    Modify the EpochOverview component

--- packages/client/components/layouts/EpochOverview.tsx
+++ packages/client/components/layouts/EpochOverview.tsx
@@ -6,106 +6,18 @@ type Props = {
 };
 
 const EpochOverview = ({ epoch }: Props) => {
+    const Epochs = () => {
+        return (
+            <div>
+                <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
+            </div>
+        );
+    };
     return (
         <div>
             <p className='uppercase text-white text-center'>epoch {epoch}</p>
             <div className='grid grid-cols-8 w-fit mx-auto gap-2 rounded-xl bg-[#FFF0A1] p-4 '>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
-                <div>
-                    <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-                </div>
+                {Array(32).fill(<Epochs />)}
             </div>
         </div>
     );
