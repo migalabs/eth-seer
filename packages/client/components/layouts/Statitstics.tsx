e1563c9c0f2c7b484c116b1fc9bf49a6216d960b
Josep Chetrit <jchetrit1999@gmail.com>
2022-12-11 18:07:47 +0100

    Implement the query on the client

--- packages/client/components/layouts/Statitstics.tsx
+++ packages/client/components/layouts/Statitstics.tsx
@@ -1,6 +1,9 @@
 import Image from 'next/image';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
+
+// Axios
+import axiosClient from '../../config/axios';
 
 // Components
 import ProgressTileBar from '../ui/ProgressTileBar';
@@ -13,44 +16,45 @@ const Card = styled.div`
 
 type Props = {};
 
+type Epoch = {
+    f_epoch: number;
+    f_slot: number;
+    f_num_att_vals: number;
+    f_num_vals: number;
+    f_att_effective_balance_eth: number;
+    f_total_effective_balance_eth: number;
+    f_missing_source: number;
+    f_missing_target: number;
+    f_missing_head: number;
+    reward_average: string;
+    max_reward_average: string;
+};
+
 const Statitstics = (props: Props) => {
-    const epochs = [
-        {
-            epoch: 100000,
-            slot: 100064,
-            blocks: 26,
-            target: 0.49,
-            source: 0.73,
-            head: 0.23,
-            attesting: 0.88,
-            eth: 23.8,
-        },
-        {
-            epoch: 99999,
-            slot: 100032,
-            blocks: 28,
-            target: 0.67,
-            source: 0.23,
-            head: 0.46,
-            attesting: 0.99,
-            eth: 14.4,
-        },
-        {
-            epoch: 99998,
-            slot: 100000,
-            blocks: 30,
-            target: 0.12,
-            source: 0.46,
-            head: 0.69,
-            attesting: 0.78,
-            eth: 40.5,
-        },
-    ];
+    const [epochs, setEpochs] = useState([]);
+
+    useEffect(() => {
+        if (epochs.length === 0) {
+            getEpochs();
+        }
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    // get epochs
+    const getEpochs = async () => {
+        try {
+            const response = await axiosClient.get('/api/validator-rewards-summary/');
+            setEpochs(response.data.epochsStats);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     const getDesktopView = () => (
         <div className='flex flex-col px-5'>
             <div className='flex gap-x-1 justify-around py-3 items-center uppercase text-lg'>
-                <p className='w-2/12'>Epoch</p>
+                <p className='w-1/12'>Epoch</p>
                 <p className='w-1/12'>Slot</p>
                 <div className='w-2/12'>
                     <Image
@@ -69,36 +73,70 @@ const Statitstics = (props: Props) => {
             </div>
 
             <div className='flex flex-col gap-y-4'>
-                {epochs.map(epoch => (
+                {epochs.map((epoch: Epoch) => (
                     <Card
-                        key={epoch.epoch}
+                        key={epoch.f_epoch}
                         className='flex gap-x-1 justify-around items-center text-lg text-black bg-[#FFF0A2] rounded-lg py-3'
                     >
-                        <p className='w-2/12'>{epoch.epoch}</p>
-                        <p className='w-2/12'>{epoch.slot}</p>
+                        <p className='w-1/12'>{epoch.f_epoch}</p>
+                        <p className='w-1/12'>{epoch.f_slot}</p>
                         <div className='w-2/12 pt-4'>
-                            <ProgressTileBar tilesFilled={epoch.blocks} totalTiles={32} />
+                            <ProgressTileBar tilesFilled={32} totalTiles={32} />
                         </div>
-                        <div className='flex gap-x-1 justify-center w-4/12'>
-                            <div className='flex-1'>
-                                <ProgressSmoothBar title='Target' bg='#D17E00' color='#FFC163' percent={epoch.target} />
+
+                        <div className='flex flex-col w-4/12 mt-7'>
+                            <div className='flex gap-x-1 justify-center '>
+                                <div className='flex-1'>
+                                    <ProgressSmoothBar
+                                        title='Target'
+                                        bg='#D17E00'
+                                        color='#FFC163'
+                                        percent={1 - epoch.f_missing_target / epoch.f_num_vals}
+                                    />
+                                </div>
+                                <div className='flex-1'>
+                                    <ProgressSmoothBar
+                                        title='Source'
+                                        bg='#00C5D1'
+                                        color='#94F9FF'
+                                        percent={1 - epoch.f_missing_source / epoch.f_num_vals}
+                                    />
+                                </div>
+                                <div className='flex-1'>
+                                    <ProgressSmoothBar
+                                        title='Head'
+                                        bg='#8F76D6'
+                                        color='#CAB8FF'
+                                        percent={1 - epoch.f_missing_head / epoch.f_num_vals}
+                                    />
+                                </div>
                             </div>
-                            <div className='flex-1'>
-                                <ProgressSmoothBar title='Source' bg='#00C5D1' color='#94F9FF' percent={epoch.source} />
-                            </div>
-                            <div className='flex-1'>
-                                <ProgressSmoothBar title='Head' bg='#8F76D6' color='#CAB8FF' percent={epoch.head} />
-                            </div>
+
+                            <p className='text-center'>
+                                {epoch.f_num_att_vals} / {epoch.f_num_vals}
+                            </p>
                         </div>
-                        <div className='w-3/12'>
+
+                        <div className='flex flex-col w-2/12 mt-7'>
                             <ProgressSmoothBar
                                 title='Attesting/total active'
                                 bg='#0016D8'
                                 color='#BDC4FF'
-                                percent={epoch.attesting}
+                                percent={epoch.f_num_att_vals / epoch.f_num_vals}
+                            />
+                            <p>
+                                {epoch.f_att_effective_balance_eth} / {epoch.f_total_effective_balance_eth}
+                            </p>
+                        </div>
+
+                        <div className='w-1/12'>
+                            <ProgressSmoothBar
+                                title='Reward'
+                                bg='#bc00d8'
+                                color='#ffbdd9'
+                                percent={Number(epoch.reward_average) / Number(epoch.max_reward_average)}
                             />
                         </div>
-                        <p className='w-1/12'>{epoch.eth} ETH</p>
                     </Card>
                 ))}
             </div>
@@ -107,46 +145,78 @@ const Statitstics = (props: Props) => {
 
     const getPhoneView = () => (
         <div className='flex flex-col gap-y-4 uppercase px-4 mt-3'>
-            {epochs.map(epoch => (
+            {epochs.map((epoch: Epoch) => (
                 <Card
-                    key={epoch.epoch}
-                    className='flex flex-col gap-y-4 justify-around items-center text-lg text-black bg-[#FFF0A2] rounded-lg py-3'
+                    key={epoch.f_epoch}
+                    className='flex flex-col gap-y-4 justify-around items-center text-lg text-black bg-[#FFF0A2] rounded-lg p-3'
                 >
-                    <p className='font-bold'>Epoch {epoch.epoch}</p>
+                    <p className='font-bold'>Epoch {epoch.f_epoch}</p>
 
-                    <div className='flex gap-x-4'>
+                    <div className='flex gap-x-4 w-full'>
                         <div>
                             <p className='uppercase'>Slot</p>
-                            <p>{epoch.epoch}</p>
+                            <p>{epoch.f_slot}</p>
                         </div>
                         <div>
-                            <ProgressTileBar tilesFilled={epoch.blocks} totalTiles={32} hasImage statsInside />
+                            <ProgressTileBar tilesFilled={32} totalTiles={32} hasImage statsInside />
                         </div>
                     </div>
-                    <div className='flex flex-col gap-y-2'>
+
+                    <div className='flex flex-col w-full gap-y-2'>
                         <p>Attestation Accuracy</p>
 
-                        <ProgressSmoothBar title='Target' bg='#D17E00' color='#FFC163' percent={epoch.target} />
+                        <ProgressSmoothBar
+                            title='Target'
+                            bg='#D17E00'
+                            color='#FFC163'
+                            percent={1 - epoch.f_missing_target / epoch.f_num_vals}
+                        />
 
-                        <ProgressSmoothBar title='Source' bg='#00C5D1' color='#94F9FF' percent={epoch.source} />
+                        <ProgressSmoothBar
+                            title='Source'
+                            bg='#00C5D1'
+                            color='#94F9FF'
+                            percent={1 - epoch.f_missing_source / epoch.f_num_vals}
+                        />
 
-                        <ProgressSmoothBar title='Head' bg='#8F76D6' color='#CAB8FF' percent={epoch.head} />
+                        <ProgressSmoothBar
+                            title='Head'
+                            bg='#8F76D6'
+                            color='#CAB8FF'
+                            percent={1 - epoch.f_missing_head / epoch.f_num_vals}
+                        />
+
+                        <p className='text-center'>
+                            {epoch.f_num_att_vals} / {epoch.f_num_vals}
+                        </p>
                     </div>
 
-                    <div className='flex flex-col gap-y-2'>
+                    <div className='flex flex-col w-full gap-y-2'>
                         <p>Balance</p>
 
                         <ProgressSmoothBar
                             title='Attesting/total active'
                             bg='#0016D8'
                             color='#BDC4FF'
-                            percent={epoch.attesting}
+                            percent={epoch.f_num_att_vals / epoch.f_num_vals}
                         />
+
+                        <p>
+                            {epoch.f_att_effective_balance_eth} / {epoch.f_total_effective_balance_eth}
+                        </p>
                     </div>
 
-                    <div className='flex gap-x-4'>
+                    <div className='flex w-full gap-x-4'>
                         <Image className='mx-auto' src='/static/images/cup.svg' alt='Blocks' width={30} height={30} />
-                        <p>{epoch.eth} ETH</p>
+
+                        <div className='flex-1'>
+                            <ProgressSmoothBar
+                                title='Reward'
+                                bg='#bc00d8'
+                                color='#ffbdd9'
+                                percent={Number(epoch.reward_average) / Number(epoch.max_reward_average)}
+                            />
+                        </div>
                     </div>
                 </Card>
             ))}
