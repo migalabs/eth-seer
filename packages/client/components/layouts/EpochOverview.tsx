cd95ac8a1ef01cd5ded2d390daf9c54dcfc0ff95
Iuri <iuri.19.05@gmail.com>
2022-12-11 15:53:08 +0100

    Build phone design

--- packages/client/components/layouts/EpochOverview.tsx
+++ packages/client/components/layouts/EpochOverview.tsx
@@ -6,19 +6,14 @@ type Props = {
 };
 
 const EpochOverview = ({ epoch }: Props) => {
-    const Epochs = () => {
-        return (
-            <div>
-                <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
-            </div>
-        );
-    };
     return (
         <div>
             <p className='uppercase text-white text-center'>epoch {epoch}</p>
-            <div className='grid grid-cols-8 w-fit mx-auto gap-2 rounded-xl bg-[#FFF0A1] p-4 '>
-                {Array(...Array(32)).map(i => (
-                    <Epochs key={i} />
+            <div className='grid grid-cols-4 md:grid-cols-8 w-fit max-h-64 overflow-scroll mx-auto gap-2 rounded-xl bg-[#FFF0A1] p-4 '>
+                {Array(...Array(32)).map((_, idx) => (
+                    <div key={idx}>
+                        <Image src='/static/images/block_avado.svg' alt='Logo' width={50} height={50} />
+                    </div>
                 ))}
             </div>
         </div>
--- packages/client/components/layouts/Statitstics.tsx
+++ packages/client/components/layouts/Statitstics.tsx
@@ -17,6 +17,7 @@ const Statitstics = (props: Props) => {
     const epochs = [
         {
             epoch: 100000,
+            slot: 100064,
             blocks: 26,
             target: 0.49,
             source: 0.73,
@@ -26,6 +27,7 @@ const Statitstics = (props: Props) => {
         },
         {
             epoch: 99999,
+            slot: 100032,
             blocks: 28,
             target: 0.67,
             source: 0.23,
@@ -35,6 +37,7 @@ const Statitstics = (props: Props) => {
         },
         {
             epoch: 99998,
+            slot: 100000,
             blocks: 30,
             target: 0.12,
             source: 0.46,
@@ -44,73 +47,117 @@ const Statitstics = (props: Props) => {
         },
     ];
 
-    return (
-        <div className='text-center text-white'>
-            <h1 className='text-5xl uppercase'>Epoch Statistics</h1>
-
-            <div className='flex flex-col px-5'>
-                <div className='flex gap-x-1 justify-around py-3 items-center uppercase text-lg'>
-                    <p className='w-2/12'>Epoch</p>
-                    <div className='w-2/12'>
-                        <Image
-                            className='mx-auto'
-                            src='/static/images/blocks_icon.svg'
-                            alt='Blocks'
-                            width={40}
-                            height={40}
-                        />
-                    </div>
-                    <p className='w-4/12'>Attestation Accuracy</p>
-                    <p className='w-3/12'>Balance</p>
-                    <div className='w-1/12'>
-                        <Image className='mx-auto' src='/static/images/cup.svg' alt='Blocks' width={30} height={30} />
-                    </div>
+    const getDesktopView = () => (
+        <div className='flex flex-col px-5'>
+            <div className='flex gap-x-1 justify-around py-3 items-center uppercase text-lg'>
+                <p className='w-2/12'>Epoch</p>
+                <p className='w-1/12'>Slot</p>
+                <div className='w-2/12'>
+                    <Image
+                        className='mx-auto'
+                        src='/static/images/blocks_icon.svg'
+                        alt='Blocks'
+                        width={40}
+                        height={40}
+                    />
                 </div>
-
-                <div className='flex flex-col gap-y-4'>
-                    {epochs.map(epoch => (
-                        <Card
-                            key={epoch.epoch}
-                            className='flex gap-x-1 justify-around items-center text-lg text-black bg-[#FFF0A2] rounded-lg py-3'
-                        >
-                            <p className='w-2/12'>{epoch.epoch}</p>
-                            <div className='w-2/12 pt-4'>
-                                <ProgressTileBar tilesFilled={epoch.blocks} totalTiles={32} />
-                            </div>
-                            <div className='flex gap-x-1 justify-center w-4/12'>
-                                <div className='flex-1'>
-                                    <ProgressSmoothBar
-                                        title='Target'
-                                        bg='#D17E00'
-                                        color='#FFC163'
-                                        percent={epoch.target}
-                                    />
-                                </div>
-                                <div className='flex-1'>
-                                    <ProgressSmoothBar
-                                        title='Source'
-                                        bg='#00C5D1'
-                                        color='#94F9FF'
-                                        percent={epoch.source}
-                                    />
-                                </div>
-                                <div className='flex-1'>
-                                    <ProgressSmoothBar title='Head' bg='#8F76D6' color='#CAB8FF' percent={epoch.head} />
-                                </div>
-                            </div>
-                            <div className='w-3/12'>
-                                <ProgressSmoothBar
-                                    title='Attesting/total active'
-                                    bg='#0016D8'
-                                    color='#BDC4FF'
-                                    percent={epoch.attesting}
-                                />
-                            </div>
-                            <p className='w-1/12'>{epoch.eth} ETH</p>
-                        </Card>
-                    ))}
+                <p className='w-4/12'>Attestation Accuracy</p>
+                <p className='w-2/12'>Balance</p>
+                <div className='w-1/12'>
+                    <Image className='mx-auto' src='/static/images/cup.svg' alt='Blocks' width={30} height={30} />
                 </div>
             </div>
+
+            <div className='flex flex-col gap-y-4'>
+                {epochs.map(epoch => (
+                    <Card
+                        key={epoch.epoch}
+                        className='flex gap-x-1 justify-around items-center text-lg text-black bg-[#FFF0A2] rounded-lg py-3'
+                    >
+                        <p className='w-2/12'>{epoch.epoch}</p>
+                        <p className='w-2/12'>{epoch.slot}</p>
+                        <div className='w-2/12 pt-4'>
+                            <ProgressTileBar tilesFilled={epoch.blocks} totalTiles={32} />
+                        </div>
+                        <div className='flex gap-x-1 justify-center w-4/12'>
+                            <div className='flex-1'>
+                                <ProgressSmoothBar title='Target' bg='#D17E00' color='#FFC163' percent={epoch.target} />
+                            </div>
+                            <div className='flex-1'>
+                                <ProgressSmoothBar title='Source' bg='#00C5D1' color='#94F9FF' percent={epoch.source} />
+                            </div>
+                            <div className='flex-1'>
+                                <ProgressSmoothBar title='Head' bg='#8F76D6' color='#CAB8FF' percent={epoch.head} />
+                            </div>
+                        </div>
+                        <div className='w-3/12'>
+                            <ProgressSmoothBar
+                                title='Attesting/total active'
+                                bg='#0016D8'
+                                color='#BDC4FF'
+                                percent={epoch.attesting}
+                            />
+                        </div>
+                        <p className='w-1/12'>{epoch.eth} ETH</p>
+                    </Card>
+                ))}
+            </div>
+        </div>
+    );
+
+    const getPhoneView = () => (
+        <div className='flex flex-col gap-y-4 uppercase px-4 mt-3'>
+            {epochs.map(epoch => (
+                <Card
+                    key={epoch.epoch}
+                    className='flex flex-col gap-y-4 justify-around items-center text-lg text-black bg-[#FFF0A2] rounded-lg py-3'
+                >
+                    <p className='font-bold'>Epoch {epoch.epoch}</p>
+
+                    <div className='flex gap-x-4'>
+                        <div>
+                            <p className='uppercase'>Slot</p>
+                            <p>{epoch.epoch}</p>
+                        </div>
+                        <div>
+                            <ProgressTileBar tilesFilled={epoch.blocks} totalTiles={32} hasImage statsInside />
+                        </div>
+                    </div>
+                    <div className='flex flex-col gap-y-2'>
+                        <p>Attestation Accuracy</p>
+
+                        <ProgressSmoothBar title='Target' bg='#D17E00' color='#FFC163' percent={epoch.target} />
+
+                        <ProgressSmoothBar title='Source' bg='#00C5D1' color='#94F9FF' percent={epoch.source} />
+
+                        <ProgressSmoothBar title='Head' bg='#8F76D6' color='#CAB8FF' percent={epoch.head} />
+                    </div>
+
+                    <div className='flex flex-col gap-y-2'>
+                        <p>Balance</p>
+
+                        <ProgressSmoothBar
+                            title='Attesting/total active'
+                            bg='#0016D8'
+                            color='#BDC4FF'
+                            percent={epoch.attesting}
+                        />
+                    </div>
+
+                    <div className='flex gap-x-4'>
+                        <Image className='mx-auto' src='/static/images/cup.svg' alt='Blocks' width={30} height={30} />
+                        <p>{epoch.eth} ETH</p>
+                    </div>
+                </Card>
+            ))}
+        </div>
+    );
+
+    return (
+        <div className='text-center text-white'>
+            <h1 className='text-3xl md:text-5xl uppercase'>Epoch Statistics</h1>
+
+            {typeof window !== 'undefined' && window.innerWidth > 768 ? getDesktopView() : getPhoneView()}
         </div>
     );
 };
