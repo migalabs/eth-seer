c28cf2c0f89e5eff33118756337426f8fb66a584
Josep Chetrit <jchetrit1999@gmail.com>
2022-12-11 16:17:22 +0100

    Create the query

--- packages/server/controllers/validator-rewards-summary.ts
+++ packages/server/controllers/validator-rewards-summary.ts
@@ -5,31 +5,28 @@ export const getEpochsStatistics = async (req: Request, res: Response) => {
 
     try {
 
-        const [ rows, count ] =
+        const [ epochsStats, rewardsStats ] =
          await Promise.all([
             pool.query(`
-                SELECT f_epoch, COUNT(CASE WHEN f_missing_source != 'false' THEN 1 ELSE null END) AS total_source, 
-                COUNT(CASE WHEN f_missing_target != 'false' THEN 1 ELSE null END) AS total_target, 
-                COUNT(CASE WHEN f_missing_head != 'false' THEN 1 ELSE null END) AS total_head
+                SELECT f_epoch, f_slot, f_num_att_vals, f_num_vals, 
+                f_att_effective_balance_eth, f_total_effective_balance_eth,
+                f_missing_source, f_missing_target, f_missing_head
+                FROM t_epoch_metrics_summary
+                ORDER BY f_epoch DESC
+                LIMIT 10             
+            `),
+            pool.query(`
+                SELECT avg(f_reward) as reward_average, avg(f_max_reward) max_reward_average, f_epoch
                 FROM t_validator_rewards_summary
                 GROUP BY f_epoch
                 ORDER BY f_epoch DESC
-                LIMIT 5;
-            `),
-            pool.query(`
-                SELECT f_epoch, COUNT(f_epoch) AS total
-                FROM t_validator_rewards_summary 
-                WHERE (f_status = 'active' OR f_status = 'slashed') AND 
-                t_validator_rewards_summary.f_max_att_reward <> 0
-                GROUP BY f_epoch
-                ORDER BY f_epoch DESC
-                LIMIT 5;
-            `),
+                LIMIT 10
+            `)
         ]);
         
         res.json({
-            rows: rows.rows,
-            count: count.rows,
+            epochsStats: epochsStats.rows,
+            rewardsStats: rewardsStats.rows,
         });
 
 
