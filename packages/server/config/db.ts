28454d8487e93930a069ad1ef4d29a87c36209c2
Iuri <iuri.19.05@gmail.com>
2022-12-22 19:51:07 +0100

    Test notifications listener

--- packages/server/config/db.ts
+++ packages/server/config/db.ts
@@ -1,4 +1,5 @@
 import Pool from 'pg';
+import createSubscriber from 'pg-listen';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -10,6 +11,20 @@ export const pool = new Pool.Pool({
     password: process.env.DB_PASSWORD || '',
     port: Number(process.env.DB_PORT) || 0
 });
+
+const subscriber = createSubscriber({ 
+    user: process.env.DB_USER || '',
+    host: process.env.DB_HOST || '',
+    database: process.env.DB_NAME || '',
+    password: process.env.DB_PASSWORD || '',
+    port: Number(process.env.DB_PORT) || 0
+});
+
+subscriber.notifications.on("new_head", (payload) => {
+    // Payload as passed to subscriber.notify() (see below)
+    console.log("Received notification in 'my-channel':", payload)
+});
+  
 
 export const dbConnection = async () => {
 
