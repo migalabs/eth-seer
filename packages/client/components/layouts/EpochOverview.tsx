ce15cb2884a26108da4b631478675797db2d8b3b
Iuri <iuri.19.05@gmail.com>
2022-12-23 18:01:13 +0100

    Add a listener for new mined blocks

--- packages/client/components/layouts/EpochOverview.tsx
+++ packages/client/components/layouts/EpochOverview.tsx
@@ -6,11 +6,10 @@ const POOLS = ['AVADO', 'BINANCE', 'COINBASE', 'HUOBI', 'KUCOIN', 'LIDO', 'PIEDA
 
 // Types
 type Block = {
-    f_val_idx: number;
-    f_proposer_slot: number;
+    f_slot: number;
     f_pool_name: string;
     f_proposed: boolean;
-    epoch: number;
+    f_epoch: number;
 };
 
 type Props = {
@@ -43,11 +42,27 @@ const EpochOverview = ({ epoch, blocks }: Props) => {
             <p className='uppercase text-white text-center'>Epoch {epoch}</p>
             <div className='grid grid-cols-4 md:grid-cols-8 w-fit max-h-64 md:max-h-full overflow-scroll md:overflow-hidden mx-auto gap-2 rounded-xl bg-[#FFF0A1] p-4'>
                 {blocks.map(block => (
-                    <div key={block.f_proposer_slot} className='group'>
+                    <div key={block.f_slot} className='group'>
                         <p className='absolute top-4 right-4 hidden group-hover:flex text-white'>{block.f_pool_name}</p>
                         {getBlockImage(block)}
                     </div>
                 ))}
+
+                {blocks.length < 32 && (
+                    <>
+                        <Image src={`/static/images/block_mining.svg`} alt='Mining block' width={50} height={50} />
+
+                        {Array.from(Array(32 - blocks.length - 1)).map((_, idx) => (
+                            <Image
+                                key={idx}
+                                src={`/static/images/block_awaiting.svg`}
+                                alt='Awaiting block'
+                                width={50}
+                                height={50}
+                            />
+                        ))}
+                    </>
+                )}
             </div>
         </div>
     );
