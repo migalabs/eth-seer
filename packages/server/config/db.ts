a2274a0f5ec438a941fe0d689907004e1fc5ce2a
Iuri <iuri.19.05@gmail.com>
2022-12-23 18:01:13 +0100

    Add a listener for new mined blocks

--- packages/server/config/db.ts
+++ packages/server/config/db.ts
@@ -1,36 +1,21 @@
-import Pool from 'pg';
-import createSubscriber from 'pg-listen';
+import { Client } from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const pool = new Pool.Pool({
+export const pgClient = new Client({
     user: process.env.DB_USER || '',
     host: process.env.DB_HOST || '',
     database: process.env.DB_NAME || '',
     password: process.env.DB_PASSWORD || '',
     port: Number(process.env.DB_PORT) || 0
 });
-
-const subscriber = createSubscriber({ 
-    user: process.env.DB_USER || '',
-    host: process.env.DB_HOST || '',
-    database: process.env.DB_NAME || '',
-    password: process.env.DB_PASSWORD || '',
-    port: Number(process.env.DB_PORT) || 0
-});
-
-subscriber.notifications.on("new_head", (payload) => {
-    // Payload as passed to subscriber.notify() (see below)
-    console.log("Received notification in 'my-channel':", payload)
-});
-  
 
 export const dbConnection = async () => {
 
     try {
 
-        await pool.connect();
+        await pgClient.connect();
 
         console.log('Database connected');
 
