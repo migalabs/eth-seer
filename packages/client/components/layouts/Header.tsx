089ee82db2cd15b3c85b0babfbf13fab352fb5c8
Iuri <iuri.19.05@gmail.com>
2022-12-30 19:46:32 +0100

    Change logo to pixeled panda

--- packages/client/components/layouts/Header.tsx
+++ packages/client/components/layouts/Header.tsx
@@ -1,12 +1,10 @@
 import Image from 'next/image';
 import React from 'react';
 
-type Props = {};
-
-const Header = (props: Props) => {
+const Header = () => {
     return (
         <div className='flex flex-row justify-start items-center p-2'>
-            <Image src='/static/images/panda-logo.svg' alt='Logo' width={50} height={50} />
+            <Image src='/static/images/panda-logo-pixeled.svg' alt='Logo' width={50} height={50} />
 
             <p className='uppercase text-white text-xs ml-2'>
                 Panda <br /> Stats
