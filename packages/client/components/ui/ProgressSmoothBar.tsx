b95d270388a8e348855d9c7d4c2ad0d39cb49163
Josep Chetrit <jchetrit1999@gmail.com>
2022-12-31 00:24:16 +0100

    Add tooltips

--- packages/client/components/ui/ProgressSmoothBar.tsx
+++ packages/client/components/ui/ProgressSmoothBar.tsx
@@ -1,23 +1,34 @@
 import React from 'react';
+import { TooltipContainer, TooltipContentContainerStats } from './Tooltips';
 
 type Props = {
     title: string;
     percent: number;
     color: string;
     bg: string;
+    tooltipLines: string[];
+    tooltipColor: string;
 };
 
-const ProgressSmoothBar = ({ title, percent, color, bg }: Props) => {
+const ProgressSmoothBar = ({ title, percent, color, bg, tooltipLines, tooltipColor }: Props) => {
     const width = Number(percent * 100).toFixed(0);
 
     return (
         <div>
             <p className='uppercase'>{title}</p>
+
             <div className={`rounded-xl p-1 w-100 h-6`} style={{ backgroundColor: bg }}>
                 <div className={`rounded-lg h-4`} style={{ backgroundColor: color, width: `${width}%` }}>
-                    <p className='text-[8px] font-bold pt-0.5' style={{ color: bg }}>
-                        {Number(percent * 100).toFixed(2)}%
-                    </p>
+                    <TooltipContainer>
+                        <p className=' font-bold' style={{ color: bg }}>
+                            {Number(percent * 100).toFixed(2)}%
+                        </p>
+                        <TooltipContentContainerStats tooltipColor={tooltipColor} colorLetter={bg}>
+                            {tooltipLines.map((tooltip, idx) => (
+                                <span key={idx}>{tooltip}</span>
+                            ))}
+                        </TooltipContentContainerStats>
+                    </TooltipContainer>
                 </div>
             </div>
         </div>
