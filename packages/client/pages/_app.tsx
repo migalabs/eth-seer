1826ab2e4338afb42dc55254751293ab9601d55e
Iuri <iuri.19.05@gmail.com>
2023-01-16 10:10:15 +0100

    Remove tabs and use spaces instead

--- packages/client/pages/_app.tsx
+++ packages/client/pages/_app.tsx
@@ -15,19 +15,19 @@ export default function App({ Component, pageProps }: AppProps) {
                 strategy='afterInteractive'
                 dangerouslySetInnerHTML={{
                     __html: `
-					window.dataLayer = window.dataLayer || [];
-					function gtag(){dataLayer.push(arguments);}
-					
-					gtag('consent', 'default', {
-						'ad_storage': 'denied',
-						'analytics_storage': 'denied'
-					});
-		  
-					(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-							new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-							j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-							'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-							})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_TAG_MANAGER}');`,
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+
+                    gtag('consent', 'default', {
+                        'ad_storage': 'denied',
+                        'analytics_storage': 'denied'
+                    });
+
+                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_TAG_MANAGER}');`,
                 }}
             />
 
@@ -38,11 +38,11 @@ export default function App({ Component, pageProps }: AppProps) {
 
             <Script id='google-analytics' strategy='afterInteractive'>
                 {`
-				window.dataLayer = window.dataLayer || [];
-				function gtag(){window.dataLayer.push(arguments);}
-				gtag('js', new Date());
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){window.dataLayer.push(arguments);}
+                    gtag('js', new Date());
 
-				gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
 				`}
             </Script>
 
@@ -52,10 +52,10 @@ export default function App({ Component, pageProps }: AppProps) {
                     strategy='afterInteractive'
                     dangerouslySetInnerHTML={{
                         __html: `
-						gtag('consent', 'update', {
-							'ad_storage': 'granted',
-							'analytics_storage': 'granted'
-						});
+                        gtag('consent', 'update', {
+                            'ad_storage': 'granted',
+                            'analytics_storage': 'granted'
+                        });
 					`,
                     }}
                 />
