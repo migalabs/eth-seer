3d1b1b8a05fda0736a22da12860dedc40335da8b
Iuri <iuri.19.05@gmail.com>
2023-01-23 23:31:43 +0100

    Create 404 Page

--- packages/client/pages/_document.tsx
+++ packages/client/pages/_document.tsx
@@ -46,13 +46,16 @@ export default function Document() {
                 <link rel='preconnect' href='https://fonts.googleapis.com' />
                 <link rel='preconnect' href='https://fonts.gstatic.com' />
                 <link href='https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap' rel='stylesheet' />
-                <meta name="twitter:card" content="summary" />
-                <meta name="twitter:site" content="@miga_labs" />
-                <meta name="twitter:image" content="http://ethseer.com/static/images/ethseer_metadata.png?4362984378" />
-                <meta property="og:url" content="http://ethseer.com/" />
-                <meta property="og:title" content="Ethseer - Ethereum blockchain live data" />
-                <meta property="og:description" content="Ethereum's network latest epochs and blocks live data and statistics are shown on Ethseer" />
-                <meta property="og:image" content="http://ethseer.com/static/images/ethseer_metadata.png?4362984378" />
+                <meta name='twitter:card' content='summary' />
+                <meta name='twitter:site' content='@miga_labs' />
+                <meta name='twitter:image' content='http://ethseer.com/static/images/ethseer_metadata.png?4362984378' />
+                <meta property='og:url' content='http://ethseer.com/' />
+                <meta property='og:title' content='Ethseer - Ethereum blockchain live data' />
+                <meta
+                    property='og:description'
+                    content="Ethereum's network latest epochs and blocks live data and statistics are shown on Ethseer"
+                />
+                <meta property='og:image' content='http://ethseer.com/static/images/ethseer_metadata.png?4362984378' />
             </Head>
 
             <body>
