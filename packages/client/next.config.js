dd84cd4a80e52c2453674ccee52cfc77c06f7b32
Iuri <iuri.19.05@gmail.com>
2023-04-10 20:01:58 +0200

    Change tooltips urls

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
-    assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || undefined,
+    assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX ? `${process.env.NEXT_PUBLIC_ASSET_PREFIX}/` : undefined,
 };
 
 module.exports = nextConfig;
