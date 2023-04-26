c85d88ea8b861512e0a67410d4252840640726a4
Tarun <tarsuno@gmail.com>
2023-04-26 09:52:46 +0200

    Activate standalone for production environment

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
     swcMinify: true,
     assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX ? `${process.env.NEXT_PUBLIC_ASSET_PREFIX}/` : undefined,
     basePath: process.env.NEXT_PUBLIC_ASSET_PREFIX || undefined,
+	output: 'standalone', //PROD: https://nextjs.org/docs/advanced-features/output-file-tracing
 };
 
 module.exports = nextConfig;
