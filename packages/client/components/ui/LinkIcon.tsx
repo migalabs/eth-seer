b533a2bd7e2826f52c4476ebc4a9241150f4774f
Iuri <iuri.19.05@gmail.com>
2023-05-01 11:19:42 +0200

    Force orange Link Icon into Validator page

--- packages/client/components/ui/LinkIcon.tsx
+++ packages/client/components/ui/LinkIcon.tsx
@@ -6,19 +6,26 @@ import ThemeModeContext from '../../contexts/theme-mode/ThemeModeContext';
 // Components
 import CustomImage from './CustomImage';
 
-const LinkIcon = () => {
+type Props = {
+    forceOrange?: boolean;
+    forceBlue?: boolean;
+};
+
+const LinkIcon = ({ forceOrange, forceBlue }: Props) => {
     // Theme Mode Context
     const { themeMode } = useContext(ThemeModeContext) || {};
 
-    return (
-        <CustomImage
-            src={themeMode?.darkMode ? '/static/images/link_blue.svg' : '/static/images/link_orange.svg'}
-            alt='Link icon'
-            width={20}
-            height={20}
-            className='mb-1'
-        />
-    );
+    const getUrl = () => {
+        if (forceOrange) {
+            return '/static/images/link_orange.svg';
+        } else if (forceBlue) {
+            return '/static/images/link_blue.svg';
+        } else {
+            return themeMode?.darkMode ? '/static/images/link_blue.svg' : '/static/images/link_orange.svg';
+        }
+    };
+
+    return <CustomImage src={getUrl()} alt='Link icon' width={20} height={20} className='mb-1' />;
 };
 
 export default LinkIcon;
