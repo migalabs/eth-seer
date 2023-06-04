21a78c1079050f0f7fa4c3d0a005b681dffc731b
Iuri <iuri.19.05@gmail.com>
2023-06-04 11:58:52 +0200

    Improve the design from Entity page

--- packages/client/pages/entity/[name].tsx
+++ packages/client/pages/entity/[name].tsx
@@ -15,13 +15,14 @@ type Props = {
     bg: string;
     color: string;
     rounded?: boolean;
+    isFixedWidth?: boolean;
 };
-const CardContent = ({ content, bg, color, rounded }: Props) => {
+const CardContent = ({ content, bg, color, rounded, isFixedWidth }: Props) => {
     return (
         <span
             className={`block uppercase border-2 px-5 ${
                 rounded ? 'rounded-2xl' : 'rounded-lg'
-            } font-bold leading-5 py-0.5 sm:py-1`}
+            } font-bold leading-5 py-0.5 sm:py-1 ${isFixedWidth ? 'w-36 sm:w-44' : ''}`}
             style={{ background: bg, borderColor: color, color: color }}
         >
             {content}
@@ -70,7 +71,7 @@ const Entity = () => {
             {entity && (
                 <div className='mx-auto max-w-[1100px]'>
                     <div
-                        className='flex mx-2 px-10 py-5 rounded-[22px] justify-between items-center gap-x-5'
+                        className='flex mx-2 px-4 sm:px-10 py-5 rounded-[22px] justify-between items-center gap-x-5'
                         style={{
                             backgroundColor: themeMode?.darkMode ? 'var(--yellow2)' : 'var(--blue1)',
                             boxShadow: themeMode?.darkMode ? 'var(--boxShadowYellow1)' : 'var(--boxShadowBlue1)',
@@ -78,12 +79,12 @@ const Entity = () => {
                     >
                         <div className='flex flex-col gap-y-2 uppercase text-black text-xl text-[8px] sm:text-[10px] mx-auto md:mx-0'>
                             <div className='flex flex-row items-center gap-x-5'>
-                                <p className='w-60'>Aggregate Balance:</p>
+                                <p className='leading-3 w-24 xs:w-44 sm:w-60'>Aggregate Balance:</p>
                                 <p className='leading-3'>{entity && entity.aggregate_balance?.toLocaleString()} ETH</p>
                             </div>
 
-                            <div className='flex flex-col sm:flex-row gap-x-5'>
-                                <p className='w-60'>Blocks:</p>
+                            <div className='flex flex-col sm:flex-row gap-x-5 gap-y-1'>
+                                <p className='w-44 sm:w-60'>Blocks:</p>
                                 <div className='flex justify-center gap-x-4 '>
                                     <CardContent
                                         content={`Proposed: ${
@@ -104,28 +105,32 @@ const Entity = () => {
                                 </div>
                             </div>
 
-                            <div className='flex flex-col gap-y-5'>
-                                <p className='w-60'>Number of Validators:</p>
-                                <div className='flex flex-col md:flex-row items-center md:justify-center gap-x-4 gap-y-2'>
+                            <div className='flex flex-col gap-y-1 xs:gap-y-5'>
+                                <p className='w-44 sm:w-60'>Number of Validators:</p>
+                                <div className='flex flex-col xl:flex-row items-center md:justify-center gap-x-4 gap-y-2'>
                                     <CardContent
                                         content={`Deposited: ${entity && entity.deposited?.toLocaleString()}`}
                                         bg='#98D3E6'
                                         color='#0080A9'
+                                        isFixedWidth
                                     />
                                     <CardContent
                                         content={`Active: ${entity && entity.active?.toLocaleString()}`}
                                         bg='#9BD8A1'
                                         color='#00720B'
+                                        isFixedWidth
                                     />
                                     <CardContent
                                         content={`Slashed: ${entity && entity.slashed?.toLocaleString()}`}
                                         bg='#EFB0B0'
                                         color='#980E0E'
+                                        isFixedWidth
                                     />
                                     <CardContent
                                         content={`Exited: ${entity && entity.exited?.toLocaleString()}`}
                                         bg='#CDA4DC'
                                         color='#5D3BBD'
+                                        isFixedWidth
                                     />
                                 </div>
                             </div>
