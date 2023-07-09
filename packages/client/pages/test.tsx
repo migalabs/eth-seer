0fcf672863af700db047212f5af5135aaee9d5c2
Iuri <iuri.19.05@gmail.com>
2023-07-09 17:21:46 +0200

    Create a page to test the new block animation with CSS

---
+++ packages/client/pages/test.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Test = () => {
+    return (
+        <div className='mt-12'>
+            <h1 className='text-center text-white text-4xl'>Test</h1>
+
+            <div className='body-cube'>
+                <div className='cube'>
+                    <div className='cube-top'></div>
+                    <div className='cube-bottom'></div>
+                    <div className='cube-left'></div>
+                    <div className='cube-right'></div>
+                    <div className='cube-front'></div>
+                    <div className='cube-back'></div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Test;
