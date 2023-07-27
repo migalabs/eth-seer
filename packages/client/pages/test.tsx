2378095f57ef626bf75218ef477169101fcfeeb4
Marina Terentii <125751323+Artentii@users.noreply.github.com>
2023-07-27 11:30:21 +0200

    Cube test css

--- packages/client/pages/test.tsx
+++ packages/client/pages/test.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 
 const Test = () => {
     return (
-        <div className='mt-12'>
-            <h1 className='text-center text-white text-4xl'>Test</h1>
-
-            <div className='body-cube'>
-                <div className='cube'>
+        <div className='body-cube'>
+            <div className='container'>
+                <div className="cube">
                     <div className='cube-top'></div>
                     <div className='cube-bottom'></div>
                     <div className='cube-left'></div>
