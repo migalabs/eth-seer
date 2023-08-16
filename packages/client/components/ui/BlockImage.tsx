041f48fb12a55a0f7f6ad2a60c40b6d8f9317c22
Iuri <iuri.19.05@gmail.com>
2023-08-16 19:27:17 +0200

    Adjusted cross image to match block

--- packages/client/components/ui/BlockImage.tsx
+++ packages/client/components/ui/BlockImage.tsx
@@ -45,8 +45,8 @@ const BlockImage = ({ poolName, proposed = true, width, height, showCheck }: Pro
                     className='absolute z-[var(--zIndexBlockImageMissed)] top-0'
                     src={`/static/images/blocks/cubes/missed.svg`}
                     alt='Missed Logo'
-                    width={50}
-                    height={50}
+                    width={width}
+                    height={width}
                 />
             )}
 
