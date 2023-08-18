be569e5cae7653599fdfd4f3bd43882aa210f4bc
Marina Terentii <125751323+Artentii@users.noreply.github.com>
2023-08-18 17:46:45 +0200

    favicon.ico updated

--- packages/client/pages/_document.tsx
+++ packages/client/pages/_document.tsx
@@ -96,7 +96,7 @@ export default function Document() {
             />
 
             <Head>
-                <link rel='shortcut icon' href={`${assetPrefix}/static/images/favicon.png`} />
+                <link rel='shortcut icon' href={`${assetPrefix}/static/images/favicon.ico`} />
                 <link rel='preconnect' href='https://fonts.googleapis.com' />
                 <link rel='preconnect' href='https://fonts.gstatic.com' />
                 <link href='https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap' rel='stylesheet' />
