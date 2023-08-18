a91ed1711435946a25e630aab052126c1e816d18
Marina Terentii <125751323+Artentii@users.noreply.github.com>
2023-08-18 11:35:34 +0200

    Next/prev buttons implemented using svg code instead of images

--- packages/client/components/layouts/ChainOverview.tsx
+++ packages/client/components/layouts/ChainOverview.tsx
@@ -6,7 +6,6 @@ import BlocksContext from '../../contexts/blocks/BlocksContext';
 
 // Components
 import EpochOverview from './EpochOverview';
-import CustomImage from '../ui/CustomImage';
 
 const ChainOverview = () => {
     // Theme Mode Context
@@ -77,16 +76,41 @@ const ChainOverview = () => {
     };
 
     return (
-        <div className='flex flex-row justify-center space-x-4 md:space-x-5 px-7'>
-            <div className='flex items-center mt-8'>
-                <CustomImage
-                    src={themeMode?.darkMode ? '/static/images/arrow.svg' : '/static/images/arrow-blue.svg'}
-                    alt='Left arrow'
-                    width={30}
-                    height={30}
-                    onClick={() => arrowLeftHidden || handleLeft()}
-                    className={`h-fit ${arrowLeftHidden ? 'opacity-0' : 'cursor-pointer'}`}
-                />
+        <div className='flex flex-row justify-center items-center gap-4'>
+            <div className='flex mt-5'>
+                {themeMode?.darkMode ? (
+                    <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        width='50'
+                        height='50'
+                        viewBox='0 0 16 16'
+                        className={`stroke-[#fbc508] stroke-1 dark-mode-class ${
+                            arrowLeftHidden ? 'opacity-0' : 'cursor-pointer'
+                        }`}
+                        onClick={() => arrowLeftHidden || handleLeft()}
+                    >
+                        <path
+                            fill-rule='evenodd'
+                            d='M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z'
+                        />
+                    </svg>
+                ) : (
+                    <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        width='50'
+                        height='50'
+                        viewBox='0 0 16 16'
+                        className={`stroke-[#6cc4e0] stroke-1 light-mode-class' ${
+                            arrowLeftHidden ? 'opacity-0' : 'cursor-pointer'
+                        }`}
+                        onClick={() => arrowLeftHidden || handleLeft()}
+                    >
+                        <path
+                            fill-rule='evenodd'
+                            d='M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z'
+                        />
+                    </svg>
+                )}
             </div>
 
             {blocks &&
@@ -105,15 +129,40 @@ const ChainOverview = () => {
                         />
                     ))}
 
-            <div className='flex items-center mt-8'>
-                <CustomImage
-                    src={themeMode?.darkMode ? '/static/images/arrow.svg' : '/static/images/arrow-blue.svg'}
-                    alt='Right arrow'
-                    width={30}
-                    height={30}
-                    onClick={() => arrowRightHidden || handleRight()}
-                    className={`h-fit rotate-180 ${arrowRightHidden ? 'opacity-0' : 'cursor-pointer'}`}
-                />
+            <div className='flex mt-5'>
+                {themeMode?.darkMode ? (
+                    <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        width='50'
+                        height='50'
+                        viewBox='0 0 16 16'
+                        className={`stroke-[#fbc508] stroke-1 dark-mode-class ${
+                            arrowRightHidden ? 'opacity-0' : 'cursor-pointer'
+                        }`}
+                        onClick={() => arrowRightHidden || handleRight()}
+                    >
+                        <path
+                            fill-rule='evenodd'
+                            d='M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z'
+                        />
+                    </svg>
+                ) : (
+                    <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        width='50'
+                        height='50'
+                        viewBox='0 0 16 16'
+                        className={`stroke-[#6cc4e0] stroke-1 light-mode-class' ${
+                            arrowRightHidden ? 'opacity-0' : 'cursor-pointer'
+                        }`}
+                        onClick={() => arrowRightHidden || handleRight()}
+                    >
+                        <path
+                            fill-rule='evenodd'
+                            d='M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z'
+                        />
+                    </svg>
+                )}
             </div>
         </div>
     );
