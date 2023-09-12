19e4758cbeddd2cb0e11dbe14e0e4c65cc0ade3f
Marina Terentii <125751323+Artentii@users.noreply.github.com>
2023-09-12 19:11:17 +0200

    Epoch description added

--- packages/client/components/layouts/Statitstics.tsx
+++ packages/client/components/layouts/Statitstics.tsx
@@ -837,7 +837,18 @@ const Statitstics = () => {
     return (
         <div className='text-center text-white'>
             <h2 className='text-lg md:text-2xl uppercase mb-5'>Epoch Statistics</h2>
-
+            <div className="mx-auto py-4 px-6 bg-white/30 border-2 border-dashed rounded-xl flex w-11/12 lg:w-3/5 mb-5">
+                <CustomImage
+                    src='/static/images/info.webp'
+                    alt='More information icon'
+                    width={50}
+                    height={50}
+                    className='object-contain relative bottom-4 right-2'
+                />
+                <h3 className='text-white text-xs text-center'>
+                Epochs in Ethereum refer to a specific period of time in the Beacon Chain. Each epoch has a duration of 6.4 minutes and is composed of 32 slots.
+                </h3>
+            </div>
             {desktopView ? getDesktopView() : getPhoneView()}
         </div>
     );
