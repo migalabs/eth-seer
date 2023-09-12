b4ed821a50b4bc01e19f4b91cbb574cefaf06414
Marina Terentii <125751323+Artentii@users.noreply.github.com>
2023-09-12 19:28:12 +0200

    Landing page description added

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -69,10 +69,14 @@ export default function Home() {
                     <div className='text-center text-white mb-5 md:mt-0 mt-20'>
                         <h1 className='text-lg md:text-2xl uppercase'>Ethereum blockchain explorer</h1>
                     </div>
+                    <div className="mx-auto py-4 px-6 bg-white/30 border-2 border-dashed rounded-xl w-11/12 mb-5">
+                        <h2 className='text-white text-xs text-center'>
+                        Ethseer.io provides information about the Beacon Chain of Ethereum. It displays the blocks in real-time being produced with a user-friendly interface and allows users to search for information in an engaging manner, simulating an arcade game to understand the Blockchain.</h2>
+                    </div>
                     <SummaryOverview />
                     <ChainOverview />
                     <div className='text-center text-white mt-3'>
-                        {/* <h2 className='text-lg md:text-xl uppercase'>Epoch Statistics</h2> */}
+                        <h2 className='text-lg md:text-xl uppercase'>Epoch Statistics</h2>
                         <Statitstics />
                     </div>
                 </Layout>
