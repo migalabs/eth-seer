c124220c4f3fdf26745a3e69c83a87c33a4a32a5
Marina Terentii <125751323+Artentii@users.noreply.github.com>
2023-09-13 11:53:35 +0200

    Update Arrow.tsx

--- packages/client/components/ui/Arrow.tsx
+++ packages/client/components/ui/Arrow.tsx
@@ -32,8 +32,8 @@ const Arrow = ({ direction, height = 30, width = 30, className, onClick }: Props
             viewBox='0 0 16 16'
             className={`stroke-1 dark-mode-class cursor-pointer ${className ?? ''}`}
             style={{
-                stroke: themeMode?.darkMode ? 'var(--yellow1)' : 'var(--blue2)',
-                fill: themeMode?.darkMode ? 'var(--yellow1)' : 'var(--blue2)',
+                stroke: themeMode?.darkMode ? 'var(--yellow1)' : 'var(--blue3)',
+                fill: themeMode?.darkMode ? 'var(--yellow1)' : 'var(--blue3)',
             }}
             onClick={onClick}
         >
