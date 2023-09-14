82852983ef1ab354ad7a5dee94993b64e921bd5e
Marina Terentii <125751323+Artentii@users.noreply.github.com>
2023-09-14 08:54:05 +0200

    Update index.tsx

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -70,7 +70,7 @@ export default function Home() {
 
                     <div className='mx-auto py-4 px-6 bg-white/30 border-2 border-dashed rounded-xl w-11/12 lg:w-10/12 mb-5'>
                         <h2 className='text-white text-xs text-center'>
-                            Ethseer.io provides information about the Beacon Chain of Ethereum. It displays the blocks
+                            EthSeer provides information about the Beacon Chain of Ethereum. It displays the blocks
                             being produced in real-time with a user-friendly interface and allows users to search for
                             information in an engaging manner to understand the Blockchain.
                         </h2>
