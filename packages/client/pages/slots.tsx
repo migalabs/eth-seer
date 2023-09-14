fe19b225627e075ad11d532a97587eea8b172b58
Marina Terentii <125751323+Artentii@users.noreply.github.com>
2023-09-14 22:04:46 +0200

    Updated font color of descriptions light mode

--- packages/client/pages/slots.tsx
+++ packages/client/pages/slots.tsx
@@ -1,5 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import Head from 'next/head';
+
+// Contexts
+import ThemeModeContext from '.././contexts/theme-mode/ThemeModeContext';
 
 // Axios
 import axiosClient from '../config/axios';
@@ -14,6 +17,9 @@ import ViewMoreButton from '../components/ui/ViewMoreButton';
 import { Slot } from '../types';
 
 const Slots = () => {
+    // Theme Mode Context
+    const { themeMode } = useContext(ThemeModeContext) ?? {};
+
     // States
     const [slots, setSlots] = useState<Slot[]>([]);
     const [currentPage, setCurrentPage] = useState(0);
@@ -68,8 +74,10 @@ const Slots = () => {
                 Slots Storing Key Blockchain State and Validator Information
             </h1>
 
-            <div className='mx-auto py-4 px-6 bg-white/30 border-2 border-dashed rounded-xl flex w-11/12 lg:w-3/5 my-3'>
-                <h2 className='text-white text-xs text-center'>
+            <div className='mx-auto py-4 px-6 bg-white/30 border-2 border-dashed rounded-xl flex w-11/12 lg:w-3/5 my-3' >
+                <h2 className='text-xs text-center' style={{
+                    color: themeMode?.darkMode ? 'var(--white)' : 'var(--newOrange)'
+                }}>
                     Every epoch is divided into regular interval called slots, which occur every 12 seconds. Slots store
                     information about the current state of the blockchain, such as the current set of validators and
                     signatures for the proposed block.
