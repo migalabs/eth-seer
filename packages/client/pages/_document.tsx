9342ced0cd63c9d706621e479ed4abc5f1f807c7
Marina Terentii <125751323+Artentii@users.noreply.github.com>
2023-09-21 17:16:13 +0200

    Update _document.tsx

--- packages/client/pages/_document.tsx
+++ packages/client/pages/_document.tsx
@@ -9,7 +9,7 @@ export default function Document() {
             <Global
                 styles={css`
                     :root {
-                        --headingFont: 'Press Start 2P', cursive;
+                        --headingFont: 'Poppins', sans-serif;
                         --brown1: #ffc163;
                         --brown2: #df8736;
                         --brown3: #ae5a0c;
@@ -45,7 +45,54 @@ export default function Document() {
                         --purple3: #bdc4ff;
                         --purple4: #886ed280;
                         --newOrange: #000000;
+
+                        //NEW DESIGN:
+
+                        //COLORS:
                         --white: #ffffff;
+                        --black: #000000;
+                        --purple: #c9b6f8;
+                        --lightGray: #a19f9f;
+                        --darkGray: #6d6d6d;
+                        --bgBar: #c9c9c9;
+                        --bgMainLightMode: #ffffff70;
+                        --bgFairLightMode: #f5f5f550;
+                        --bgStrongLightMode: #bdbdbd50;
+                        --bgDarkMode: #343434;
+                        --bgFairDarkMode: #5b5b5b65;
+                        --bgStrongDarkMode: #30303080;
+                        --proposedGreen: #53945a;
+                        --missedRed: #e86666;
+                        --depositedOrange: #f69a2e;
+                        --depositedBlue: #4caee5;
+                        --exitedPurple: #a966c1;
+
+                        //SHADOWS:
+                        --boxShadowGreen: 0px 2px 4px 0px #3b503d inset;
+                        --boxShadowRed: 0px 2px 4px 0px #5e2c2c inset;
+                        --boxShadowOrange: 0px 2px 4px 0px #735428 inset;
+                        --boxShadowBlue: 0px 2px 4px 0px #2f78a1 inset;
+                        --boxShadowPurple: 0px 2px 4px 0px #512b5f inset;
+                        --boxShadowCardDark: 0px 4px 4px 0px rgba(0, 0, 0, 0.5) inset;
+                        --boxShadowCardLight: 0px 4px 4px 0px rgba(0, 0, 0, 0.15) inset;
+
+                        //BACKGROUND:
+                        --background-light: linear-gradient(
+                            180deg,
+                            rgba(192, 175, 238, 1) 0%,
+                            rgba(215, 200, 255, 1) 24%,
+                            rgba(216, 200, 255, 1) 52%,
+                            rgba(235, 227, 255, 1) 74%,
+                            rgba(253, 253, 255, 1) 100%
+                        );
+                        --background-dark: linear-gradient(
+                            180deg,
+                            rgba(52, 52, 52, 1) 0%,
+                            rgba(65, 65, 65, 1) 33%,
+                            rgba(112, 112, 112, 1) 60%,
+                            rgba(199, 199, 199, 1) 85%,
+                            rgba(231, 223, 255, 1) 99%
+                        );
 
                         --boxShadowYellow1: inset -7px -7px 8px var(--yellow3), inset 7px 7px 8px var(--yellow3);
                         --boxShadowYellow2: inset -2.8px -2.8px 3.2px var(--yellow4),
@@ -66,21 +113,6 @@ export default function Document() {
                         --boxShadowGreen3: inset 4px 4px 4px var(--green3), inset -4px -4px 4px var(--green3);
                         --boxShadowPurple1: inset -7px -7px 8px var(--purple2), inset 7px 7px 8px var(--purple2);
                         --boxShadowPurple2: inset -4px -4px 4px var(--purple4), inset 4px 4px 4px var(--purple4);
-
-                        --background-light: radial-gradient(
-                            circle,
-                            rgba(255, 161, 72, 1) 0%,
-                            rgba(255, 194, 127, 1) 47%,
-                            rgba(255, 224, 187, 1) 82%,
-                            rgba(255, 239, 219, 1) 100%
-                        );
-                        --background-dark: radial-gradient(
-                            circle,
-                            rgba(141, 72, 231, 1) 0%,
-                            rgba(67, 10, 145, 1) 32%,
-                            rgba(7, 7, 97, 1) 60%,
-                            rgba(14, 1, 31, 1) 100%
-                        );
 
                         --zIndexThemeModeSwitch: 5;
                         --zIndexBlockImageMissed: 10;
@@ -120,7 +152,10 @@ export default function Document() {
                 <link rel='shortcut icon' href={`${assetPrefix}/static/images/favicon.ico`} />
                 <link rel='preconnect' href='https://fonts.googleapis.com' />
                 <link rel='preconnect' href='https://fonts.gstatic.com' />
-                <link href='https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap' rel='stylesheet' />
+                <link
+                    href='https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&display=swap'
+                    rel='stylesheet'
+                />
                 <meta
                     name='keywords'
                     content='ethereum, ethereum blockchain explorer, ethereum block explorer, search'
