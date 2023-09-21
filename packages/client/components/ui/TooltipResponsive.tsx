87959e80405540e18c0c0c2a0d2b620a607afc9b
Marina Terentii <125751323+Artentii@users.noreply.github.com>
2023-09-22 01:36:30 +0200

    Update TooltipResponsive.tsx

--- packages/client/components/ui/TooltipResponsive.tsx
+++ packages/client/components/ui/TooltipResponsive.tsx
@@ -31,7 +31,7 @@ const TooltipResponsive = ({ width, backgroundColor, colorLetter, content, top, 
 
     return (
         <div
-            className='absolute flex-col border-2 rounded-2xl py-4 px-4 mt-2 mx-auto hidden z-[var(--zIndexTooltip)] text-[8px] uppercase leading-4'
+            className='absolute flex-col border-2 rounded-2xl py-4 px-4 mt-2 mx-auto hidden z-[var(--zIndexTooltip)] text-[12px] uppercase leading-4'
             style={{
                 width,
                 left: GetParentLeftPosition(),
