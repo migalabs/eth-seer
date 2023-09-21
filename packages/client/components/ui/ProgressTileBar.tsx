76b54ffebc04ae2a1596a3d972f668175c4aae10
Marina Terentii <125751323+Artentii@users.noreply.github.com>
2023-09-22 01:28:51 +0200

    Update ProgressTileBar.tsx

--- packages/client/components/ui/ProgressTileBar.tsx
+++ packages/client/components/ui/ProgressTileBar.tsx
@@ -23,20 +23,20 @@ const ProgressTileBar = ({ tooltipContent, totalBlocks }: Props) => {
     return (
         <div className='flex flex-col gap-y-1'>
             <TooltipContainer>
-                <div className='flex gap-x-2 bg-[#FEE351] px-2 py-1.5 w-fit rounded-2xl border-2 border-[#D88D1C] mx-auto'>
+                <div className='flex gap-x-2 bg-[#c9c9c9] px-2 py-1.5 w-fit rounded-md mx-auto'>
                     <div className='flex flex-col justify-center'>
                         <div className='flex gap-x-px'>
                             {totalBlocks?.map((element, idx) => (
                                 <div
                                     key={idx}
-                                    className={`w-[3px] h-2.5 ${element === 1 ? 'bg-green-500' : 'bg-red-500'}`}
+                                    className={`w-[3px] h-2.5 ${element === 1 ? 'bg-[#6D6D6D]' : 'bg-black'}`}
                                 />
                             ))}
                             {totalBlocks === undefined &&
                                 staticArray.map((element, idx) => (
                                     <div
                                         key={idx}
-                                        className={`w-[3px] h-2.5 ${element === 1 ? 'bg-green-500' : 'bg-red-500'}`}
+                                        className={`w-[3px] h-2.5 ${element === 1 ? 'bg-[#6D6D6D]' : 'bg-black'}`}
                                     />
                                 ))}
 
