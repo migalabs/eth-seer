4117a491dfaaba3dd5cebd8e553db9c9ea3f5e04
Marina Terentii <125751323+Artentii@users.noreply.github.com>
2023-09-26 19:48:18 +0200

    Revert "Update SEO landing page"
    This reverts commit ddabf92f5430525d078aa430293485e9ef311f61.

--- packages/client/pages/entities.tsx
+++ packages/client/pages/entities.tsx
@@ -18,29 +18,27 @@ const Entities = () => {
     return (
         <Layout hideMetaDescription>
             <Head>
-                <title>Staking Entities of the Ethereum Beacon Chain - EthSeer.io</title>
+                <title>Staking Entities - EthSeer</title>
                 <meta
                     name='description'
-                    content="Explore the larger Ethereum blockchain entities that run validators. You will find their number of validators and their performance over the last week."
+                    content="Explore blockchain entities and Ethseer's role in gathering essential data, from validators to Ethereum accounts."
                 />
-                <meta name='keywords' content='Ethereum, Staking, Pool, Validators, Slashing, PoS' />
-                <link rel='canonical' href='https://ethseer.io/entities' />
+                <meta name='keywords' content='ethereum, staking, pool, validators, slashing, PoS' />
             </Head>
 
-            <h1 className='text-[30px] md:text-[40px] text-center capitalize font-semibold text-black mt-10 mb-4 md:mb-4 md:mt-0'>Ethereum Staking Entities</h1>
+            <h1 className='uppercase text-center text-3xl text-white mt-14 xl:mt-0'>Ethereum Staking Entities</h1>
 
-            <div className='mx-auto py-4 px-6 text-[12px] md:text-[16px] border rounded-md w-11/12 lg:w-10/12 mb-5'
-                        style={{
-                            background: themeMode?.darkMode ? 'var(--bgDarkMode)' : 'var(--bgMainLightMode)',
-                            borderColor: themeMode?.darkMode ? 'var(--white)' : 'var(--lightGray)',
-                        }}>
+            <div className='mx-auto py-4 px-6 bg-white/30 border-2 border-dashed rounded-xl flex w-11/12 lg:w-3/5 my-3'>
                 <h2
-                    className='text-center'
+                    className='text-xs text-center'
                     style={{
-                        color: themeMode?.darkMode ? 'var(--white)' : 'var(--black)',
+                        color: themeMode?.darkMode ? 'var(--white)' : 'var(--newOrange)',
                     }}
                 >
-                    An entity can range from an individual running many validators to an institutional organization providing staking services. If it is running at least 100 validators, we catalog it as an entity. Ethseer obtains information about the entities to which validators belong to through graffiti, deposit address analysis, among others. EthSeer also monitors their performance.
+                    An entity can range from an individual running many validators to an institutional organization
+                    providing staking services. If it is running at least 100 validators, we catalog it as an entity.
+                    Ethseer obtains information about the entities to which validators belong to through graffiti,
+                    deposit address analysis, among others. EthSeer also monitors their performance.
                 </h2>
             </div>
 
