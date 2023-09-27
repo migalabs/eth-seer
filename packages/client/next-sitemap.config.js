36ae03844df9b5b0a1de82e307ffc598193d695e
Iuri <iuri.19.05@gmail.com>
2023-09-27 20:32:56 +0200

    Change sitemap.xml and robots.txt

--- packages/client/next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-const siteUrl = process.env.NEXT_PUBLIC_SITE_URL;
-
-module.exports = {
-    siteUrl,
-    generateRobotsTxt: true,
-    generateIndexSitemap: false,
-};
--- packages/client/pages/slots.tsx
+++ packages/client/pages/slots.tsx
@@ -72,16 +72,10 @@ const Slots = () => {
                 <link rel='canonical' href='https://ethseer.io/slots' />
             </Head>
 
-            <h1 className='text-white text-center text-xl md:text-3xl uppercase mt-14 xl:mt-0'>
-                Ethereum Slots
-            </h1>
+            <h1 className='text-white text-center text-xl md:text-3xl uppercase mt-14 xl:mt-0'>Ethereum Slots</h1>
 
-            <div
-                className='mx-auto py-4 px-6 bg-white/30 border-2 border-dashed rounded-xl flex w-11/12 lg:w-10/12 my-3'
-            >
-                <h2
-                    className='text-white text-xs text-center'
-                >
+            <div className='mx-auto py-4 px-6 bg-white/30 border-2 border-dashed rounded-xl flex w-11/12 lg:w-10/12 my-3'>
+                <h2 className='text-white text-xs text-center'>
                     Every epoch is divided into regular interval called slots, which occur every 12 seconds. At every
                     slot, one validator can propose a block, and the other validators need to attest on the canonical
                     chain.
