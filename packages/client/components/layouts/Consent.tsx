673f319e4adb89c761fcc23059b6a4d9ce0dc26c
Marina Terentii <125751323+Artentii@users.noreply.github.com>
2023-10-02 16:08:53 +0200

    Cookies style updated

--- packages/client/components/layouts/Consent.tsx
+++ packages/client/components/layouts/Consent.tsx
@@ -1,6 +1,9 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, useContext } from 'react';
 import { setCookie, hasCookie } from 'cookies-next';
 import styled from '@emotion/styled';
+
+//Context
+import ThemeModeContext from '../../contexts/theme-mode/ThemeModeContext';
 
 // Components
 import CustomImage from '../ui/CustomImage';
@@ -12,80 +15,48 @@ type Props = {
 
 const Screen = styled.div<Props>`
     display: ${props => (props.consent ? 'none' : 'block')};
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: calc(100vw - 17px);
-    height: 100vh;
+    position: absolute;
+    top: 70%;
+    right: 50%;
+    transform: translate(-50%, -50%);
     z-index: 40;
 `;
 
 const Container = styled.div<Props>`
     display: ${props => (props.consent ? 'none' : 'block')};
     position: absolute;
-    top: calc(50% - 175px);
-    left: 2.7%;
-    height: 350px;
-    width: 100%;
-    padding: 10px 20px;
     text-align: center;
-    background-color: var(--brown1);
-    box-shadow: inset 3px 3px 5px var(--brown3), inset -3px -3px 5px var(--brown3);
-    border-radius: 1.5rem;
+    background-color: white;
+    border-radius: .5rem;
+    border: 2px #c9b6f8 solid;
     z-index: var(--zIndexConsent);
-    animation-duration: 1s;
-    animation-name: slidein;
-
-    @keyframes slidein {
-        from {
-            opacity: 0;
-            transform: scale(0.8);
-        }
-
-        to {
-            opacity: 1;
-            transform: scale(1);
-        }
-    }
-
-    .text {
-        font-family: var(--headingFont);
-        font-size: 12px;
-    }
 
     .buttons-container {
         display: flex;
         flex-direction: column;
         justify-content: center;
+        align-items: center;
         row-gap: 5px;
-        margin: 10px 0;
+        margin: 5px 0;
     }
 
     button {
-        box-shadow: inset 3px 3px 5px var(--brown3), inset -3px -3px 5px var(--brown3);
-        border-radius: 40px;
-        font-size: 10px;
-        text-transform: uppercase;
-        font-family: var(--headingFont);
-        font-style: normal;
-        font-weight: 500;
-        padding: 15px 20px;
+        padding: 10px 20px;
         cursor: pointer;
-        background-color: var(--brown4);
-        color: white;
+        background-color: var(--purple);
+        color: black;
+        border-radius: .25rem;
 
         &:hover {
-            box-shadow: inset 3px 3px 5px var(--brown5), inset -3px -3px 5px var(--brown5);
-            background-color: var(--yellow1);
+            background-color: var(--lightGray);
+            color: white;
+            transition: all .2s ease-in-out;
         }
     }
 
     @media (min-width: 768px) {
-        top: calc(50% - 185px);
-        left: calc(50% - 350px);
-        height: 370px;
         width: 700px;
-        padding: 20px 50px;
+        padding: 10px 20px;
 
         .text {
             font-size: 16px;
@@ -113,6 +84,9 @@ const Consent = () => {
         setConsent(hasCookie('localConsent'));
     }, []);
 
+    // Theme Mode Context
+    const { themeMode } = React.useContext(ThemeModeContext) ?? {};
+
     const acceptCookie = () => {
         setConsent(true);
         setCookie('localConsent', 'true', { maxAge: 60 * 60 * 24 * 365 });
@@ -122,24 +96,6 @@ const Consent = () => {
         });
     };
 
-    const handleMouseEnterNecessary = () => {
-        setGeneralVisible(false);
-        setNecessaryVisible(true);
-        setAllVisible(false);
-    };
-
-    const handleMouseLeave = () => {
-        setGeneralVisible(true);
-        setNecessaryVisible(false);
-        setAllVisible(false);
-    };
-
-    const handleMouseEnterAll = () => {
-        setGeneralVisible(false);
-        setNecessaryVisible(false);
-        setAllVisible(true);
-    };
-
     if (consent === true) {
         return null;
     }
@@ -147,65 +103,31 @@ const Consent = () => {
     return (
         <Screen consent={consent}>
             <Container consent={consent}>
-                <div className='flex flex-col justify-between h-full'>
-                    <div>
-                        <div className={generalVisisble ? 'flex' : 'hidden'}>
+                <div>
+                    <div className="flex flex-col items-start">
+                        <div className="flex flex-row justify-start items-center mb-2 gap-2">
                             <CustomImage
-                                src='/static/images/cookies/cookies.webp'
-                                alt='Pacman on its way to eat and accept some cookies'
-                                width={400}
-                                height={400}
-                                className='mx-auto'
-                            />
+                                    src='/static/images/cookies/cookie.webp'
+                                    alt='Cookie'
+                                    width={40}
+                                    height={40}
+                                    className='mx-auto'
+                                />
+                            <p className='capitalize font-medium text-[14px] md:text-[20px]'>cookies</p>
                         </div>
-                        <div className={`relative ${necessaryVisible ? 'block' : 'hidden'}`}>
-                            <CustomImage
-                                src='/static/images/cookies/necessary_cookies.webp'
-                                alt='Pacman eating the necessary amount of cookies '
-                                width={180}
-                                height={180}
-                                className='mx-auto'
-                            />
-                        </div>
-                        <div className={`relative ${allVisible ? 'block' : 'hidden'}`}>
-                            <CustomImage
-                                src='/static/images/cookies/all_cookies.webp'
-                                alt='Happy pacman accepting all cookies'
-                                width={180}
-                                height={180}
-                                className='mx-auto'
-                            />
-                        </div>
-                    </div>
-
-                    <div>
-                        <p className={`text ${generalVisisble ? 'flex' : 'hidden'}`}>
+                        <p className={`text-left text-[12px] md:text-[16px] text ${generalVisisble ? 'flex' : 'hidden'}`}>
                             We use cookies to know how users interact with our website to deliver a better user
                             experience.
                         </p>
-
-                        <p className={`text ${necessaryVisible ? 'block' : 'hidden'}`}>
-                            YUM! Just the necessary amount of cookies.
-                        </p>
-
-                        <p className={`text ${allVisible ? 'block' : 'hidden'}`}>
-                            We care about your data. Thanks for helping us improving the user experience on our website.
-                            Enjoy!
-                        </p>
                     </div>
-
                     <div className='buttons-container'>
                         <button
                             onClick={acceptCookie}
-                            onMouseEnter={handleMouseEnterNecessary}
-                            onMouseLeave={handleMouseLeave}
                         >
                             Only necessary
                         </button>
                         <button
                             onClick={acceptCookie}
-                            onMouseEnter={handleMouseEnterAll}
-                            onMouseLeave={handleMouseLeave}
                         >
                             Accept all cookies
                         </button>
