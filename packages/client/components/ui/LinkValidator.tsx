e0235e62336c085bdf2812505862d87f591d4eff
Josep Chetrit <jchetrit1999@gmail.com>
2023-10-04 06:43:56 +0200

    fix conflict error

--- packages/client/components/ui/LinkValidator.tsx
+++ packages/client/components/ui/LinkValidator.tsx
@@ -25,15 +25,10 @@ const LinkValidator = ({ validator, children, mxAuto }: Props) => {
         <NetworkLink
             href={`/validator/${validator}`}
             passHref
-<<<<<<< HEAD
-            as={`/validators/${validator}`}
             className={`flex gap-x-1 items-center font-medium md:hover:underline underline-offset-4 decoration-2 w-fit ${
                 mxAuto ? 'mx-auto' : ''
             }`}
             style={baseStyle}
-=======
-            className={`flex gap-x-1 items-center w-fit ${mxAuto ? 'mx-auto' : ''}`}
->>>>>>> ea71594 (Create [network] folder and adapt all next/Link)
         >
             {children ?? (
                 <>
