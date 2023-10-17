26a23df216fc9b97c50a6dd9b4fd31a09bfe6d07
Marina Terentii <125751323+Artentii@users.noreply.github.com>
2023-10-17 17:47:51 +0200

    Update _document.tsx

--- packages/client/pages/_document.tsx
+++ packages/client/pages/_document.tsx
@@ -10,43 +10,6 @@ export default function Document() {
                 styles={css`
                     :root {
                         --headingFont: 'Poppins', sans-serif;
-                        --brown1: #ffc163;
-                        --brown2: #df8736;
-                        --brown3: #ae5a0c;
-                        --brown4: #c57f18;
-                        --brown5: #b38b02;
-                        --yellow1: #fbc508;
-                        --yellow2: #fff0a1;
-                        --yellow3: #edbf4a;
-                        --yellow4: #f0c83a;
-                        --yellow5: #9a7b2d;
-                        --yellow6: #fff0a1b3;
-                        --orange1: #ec903c;
-                        --orange2: #ffcea1;
-                        --orange3: #f18d30;
-                        --orange4: #ffb16866;
-                        --blue1: #fff0dd;
-                        --blue2: #ffd19b;
-                        --blue3: #ffce99;
-                        --blue4: #ecb77b;
-                        --blue5: #cd8f47;
-                        --blue6: #cb8e46;
-                        --blue7: #0080a9;
-                        --blue8: #0080a94d;
-                        --blue9: #2b6d83;
-                        --blue10: #209198;
-                        --blue11: #c6ecf8b3;
-                        --green1: #a7eed466;
-                        --green2: #a7eed4;
-                        --green3: #29c68e;
-                        --green4: #89ebc8;
-                        --purple1: #bdc4ff99;
-                        --purple2: #886ed2;
-                        --purple3: #bdc4ff;
-                        --purple4: #886ed280;
-                        --newOrange: #000000;
-
-                        //NEW DESIGN:
 
                         //COLORS:
                         --white: #ffffff;
@@ -88,33 +51,13 @@ export default function Document() {
                         );
                         --background-dark: linear-gradient(
                             180deg,
-                            rgba(203, 184, 250, 1) 0%,
-                            rgba(145, 132, 179, 1) 16%,
-                            rgba(103, 92, 130, 1) 38%,
-                            rgba(85, 85, 85, 1) 61%,
-                            rgba(52, 52, 52, 1) 87%,
-                            rgba(52, 52, 52, 1) 100%
+                            rgba(169, 154, 207, 1) 0%,
+                            rgba(121, 107, 156, 1) 16%,
+                            rgba(78, 67, 103, 1) 38%,
+                            rgba(54, 45, 74, 1) 61%,
+                            rgba(35, 28, 51, 1) 87%,
+                            rgba(0, 0, 0, 1) 100%
                         );
-
-                        --boxShadowYellow1: inset -7px -7px 8px var(--yellow3), inset 7px 7px 8px var(--yellow3);
-                        --boxShadowYellow2: inset -2.8px -2.8px 3.2px var(--yellow4),
-                            inset 2.8px 2.8px 3.2px var(--yellow4);
-                        --boxShadowYellow3: inset -2.76px -2.76px 3.15px var(--yellow3),
-                            inset 2.76px 2.76px 3.15px var(--yellow3);
-                        --boxShadowOrange1: inset -7px -7px 8px var(--orange1), inset 7px 7px 8px var(--orange1);
-                        --boxShadowOrange2: inset -7px -7px 8px var(--orange2), inset 7px 7px 8px var(--orange2);
-                        --boxShadowOrange3: inset -4px -4px 4px var(--orange3), inset 4px 4px 4px var(--orange3);
-                        --boxShadowBlue1: inset -5px -5px 4px var(--blue2), inset 5px 5px 4px var(--blue2);
-                        --boxShadowBlue2: inset -5px -5px 4px var(--blue6), inset 5px 5px 4px var(--blue6);
-                        --boxShadowBlue3: inset -7px -7px 8px var(--blue3), inset 7px 7px 8px var(--blue3);
-                        --boxShadowBlue4: inset 4px 4px 4px var(--blue4), inset -4px -4px 4px var(--blue4);
-                        --boxShadowBlue5: inset -2.76px -2.76px 3.15px var(--blue6),
-                            inset 2.76px 2.76px 3.15px var(--blue6);
-                        --boxShadowGreen1: inset -7px -7px 8px var(--green2), inset 7px 7px 8px var(--green2);
-                        --boxShadowGreen2: inset -7px -7px 8px var(--green3), inset 7px 7px 8px var(--green3);
-                        --boxShadowGreen3: inset 4px 4px 4px var(--green3), inset -4px -4px 4px var(--green3);
-                        --boxShadowPurple1: inset -7px -7px 8px var(--purple2), inset 7px 7px 8px var(--purple2);
-                        --boxShadowPurple2: inset -4px -4px 4px var(--purple4), inset 4px 4px 4px var(--purple4);
 
                         --zIndexThemeModeSwitch: 5;
                         --zIndexBlockImageMissed: 10;
@@ -138,7 +81,7 @@ export default function Document() {
                         background-image: var(--background-light);
                         background-repeat: no-repeat;
                         background-size: cover;
-                        background-attachment: fixed;
+                        background-attachment: scroll;
                         font-size: 1.6rem;
                         font-family: var(--headingFont);
                     }
