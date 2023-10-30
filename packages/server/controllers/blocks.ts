c80a47f8bb95e61facbef3ec2faf309706ed42f4
Josep Chetrit <jchetrit1999@gmail.com>
2023-10-30 22:05:03 +0100

    implement queries for block page

---
+++ packages/server/controllers/blocks.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from 'express';
+import { pgPools, pgListeners } from '../config/db';
+
+export const getBlockById = async (req: Request, res: Response) => {
+
+    try {
+
+        const { id } = req.params;
+        const { network } = req.query;
+
+        const pgPool = pgPools[network as string];
+
+        const [ block ] = 
+            await Promise.all([
+                pgPool.query(`
+                    SELECT f_timestamp, f_slot, f_epoch
+                    f_el_fee_recp, f_el_gas_limit, f_el_gas_used,
+                    f_el_transactions, f_el_block_hash, f_payload_size_bytes
+                    FROM t_block_metrics
+                    WHERE f_el_block_number = '${id}'
+                `)          
+            ]);
+        // 18022299
+       
+    
+        res.json({
+            block: {
+                ...block.rows[0],
+            },
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            msg: 'An error occurred on the server'
+        });
+    }
+};
