8241c9269fdcb56a80941df61609f3f502f600c5
Marina Terentii <125751323+Artentii@users.noreply.github.com>
2023-11-10 10:18:10 +0100

    Fixed pagination overview

--- packages/client/components/ui/Pagination.tsx
+++ packages/client/components/ui/Pagination.tsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { IconButton, Typography } from '@material-tailwind/react';
 import { ArrowRightIcon, ArrowLeftIcon } from '@heroicons/react/24/outline';
 
-export function Pagination() {
-    const [active, setActive] = React.useState(1);
+// Contexts
+import ThemeModeContext from '../../contexts/theme-mode/ThemeModeContext';
+
+interface PaginationProps {
+    active: number;
+    onChangePage: (newPage: number) => void;
+}
+
+export function Pagination({ active, onChangePage }: PaginationProps) {
+    // Theme Mode Context
+    const { themeMode } = useContext(ThemeModeContext) ?? {};
 
     const next = () => {
-        if (active === 10) return;
-
-        setActive(active + 1);
+        try {
+            onChangePage(active + 1);
+        } catch (error) {
+            console.error('Error in next:', error);
+        }
     };
 
     const prev = () => {
-        if (active === 1) return;
-
-        setActive(active - 1);
+        onChangePage(active - 1);
     };
 
     return (
@@ -24,19 +33,32 @@ export function Pagination() {
                 variant='outlined'
                 onClick={prev}
                 disabled={active === 1}
+                style={{
+                    borderColor: themeMode?.darkMode ? 'var(--white)' : 'var(--black)',
+                    color: themeMode?.darkMode ? 'var(--white)' : 'var(--black)',
+                    borderWidth: '2px',
+                }}
                 className='flex items-center justify-center'
             >
                 <ArrowLeftIcon strokeWidth={2} className='h-4 w-4' />
             </IconButton>
-            <Typography color='gray' className='font-normal text-[16px]'>
-                Page <strong className='text-gray-900'>{active}</strong> of{' '}
-                <strong className='text-gray-900'>10</strong>
+            <Typography
+                style={{ color: themeMode?.darkMode ? 'var(--white)' : 'var(--black)' }}
+                className='font-normal text-[16px]'
+            >
+                Page <strong style={{ color: themeMode?.darkMode ? 'var(--white)' : 'var(--black)' }}>{active}</strong>{' '}
+                of <strong style={{ color: themeMode?.darkMode ? 'var(--white)' : 'var(--black)' }}>10</strong>
             </Typography>
             <IconButton
                 size='sm'
                 variant='outlined'
                 onClick={next}
                 disabled={active === 10}
+                style={{
+                    borderColor: themeMode?.darkMode ? 'var(--white)' : 'var(--black)',
+                    color: themeMode?.darkMode ? 'var(--white)' : 'var(--black)',
+                    borderWidth: '2px',
+                }}
                 className='flex items-center justify-center'
             >
                 <ArrowRightIcon strokeWidth={2} className='h-4 w-4' />
