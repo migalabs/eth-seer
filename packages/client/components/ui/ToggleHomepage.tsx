6e682984aa11a2bc4f9a1cc9a1b3775e3be6f4ca
Iuri <iuri.19.05@gmail.com>
2023-12-20 20:06:35 +0100

    Refactor code and make toggle responsive

--- packages/client/components/ui/ToggleHomepage.tsx
+++ packages/client/components/ui/ToggleHomepage.tsx
@@ -1,27 +1,48 @@
+import React, { useContext } from 'react';
 import { motion } from 'framer-motion';
-import { useState } from 'react';
+
+// Context
+import ThemeModeContext from '../../contexts/theme-mode/ThemeModeContext';
 
 type Props = {
     showClient: boolean;
-    setShowClient: (show: boolean) => void;
+    onToggle: () => void;
 };
-const ToggleHomepage = ({ setShowClient, showClient }: Props) => {
+
+const ToggleHomepage = ({ showClient, onToggle }: Props) => {
+    // Theme Mode Context
+    const { themeMode } = useContext(ThemeModeContext) ?? {};
+
     return (
         <div className='flex justify-center items-center gap-2 font-medium'>
-            <span className='text-[16px] text-[var(--darkGray)]'>CL Clients</span>
-            <div
-                onClick={() => setShowClient(!showClient)}
-                className={`flex h-6 w-12 cursor-pointer rounded-full p-[2px] ${
+            <span
+                className='text-[12px] md:text-[14px]'
+                style={{ color: themeMode?.darkMode ? 'var(--white)' : 'var(--darkGray)' }}
+            >
+                CL Clients
+            </span>
+
+            <button
+                onClick={onToggle}
+                className={`flex h-5 md:h-6 w-10 md:w-12 cursor-pointer rounded-full p-[2px] ${
                     showClient ? 'bg-white justify-start' : 'bg-[var(--darkGray)] justify-end'
                 }`}
             >
                 <motion.div
-                    className={`h-5 w-5 rounded-full ${showClient ? ' bg-[var(--darkGray)]' : 'bg-white'}`}
+                    className={`h-4 md:h-5 w-4 md:w-5 rounded-full ${
+                        showClient ? ' bg-[var(--darkGray)]' : 'bg-white'
+                    }`}
                     layout
                     transition={{ type: 'spring', stiffness: 700, damping: 30 }}
                 />
-            </div>
-            <span className='text-[16px] text-[var(--darkGray)]'>Entities</span>
+            </button>
+
+            <span
+                className='text-[12px] md:text-[14px]'
+                style={{ color: themeMode?.darkMode ? 'var(--white)' : 'var(--darkGray)' }}
+            >
+                Entities
+            </span>
         </div>
     );
 };
