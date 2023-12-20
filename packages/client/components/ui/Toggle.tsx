5405bad4377382b8efa4f4902e6546776c6aeff9
Iuri <iuri.19.05@gmail.com>
2023-12-20 20:11:41 +0100

    Make Toggle generic

--- packages/client/components/ui/Toggle.tsx
+++ packages/client/components/ui/Toggle.tsx
@@ -5,11 +5,13 @@ import { motion } from 'framer-motion';
 import ThemeModeContext from '../../contexts/theme-mode/ThemeModeContext';
 
 type Props = {
-    showClient: boolean;
+    value: boolean;
+    labelLeft: string;
+    labelRight: string;
     onToggle: () => void;
 };
 
-const ToggleHomepage = ({ showClient, onToggle }: Props) => {
+const Toggle = ({ value, labelLeft, labelRight, onToggle }: Props) => {
     // Theme Mode Context
     const { themeMode } = useContext(ThemeModeContext) ?? {};
 
@@ -19,19 +21,17 @@ const ToggleHomepage = ({ showClient, onToggle }: Props) => {
                 className='text-[12px] md:text-[14px]'
                 style={{ color: themeMode?.darkMode ? 'var(--white)' : 'var(--darkGray)' }}
             >
-                CL Clients
+                {labelLeft}
             </span>
 
             <button
                 onClick={onToggle}
                 className={`flex h-5 md:h-6 w-10 md:w-12 cursor-pointer rounded-full p-[2px] ${
-                    showClient ? 'bg-white justify-start' : 'bg-[var(--darkGray)] justify-end'
+                    value ? 'bg-white justify-start' : 'bg-[var(--darkGray)] justify-end'
                 }`}
             >
                 <motion.div
-                    className={`h-4 md:h-5 w-4 md:w-5 rounded-full ${
-                        showClient ? ' bg-[var(--darkGray)]' : 'bg-white'
-                    }`}
+                    className={`h-4 md:h-5 w-4 md:w-5 rounded-full ${value ? ' bg-[var(--darkGray)]' : 'bg-white'}`}
                     layout
                     transition={{ type: 'spring', stiffness: 700, damping: 30 }}
                 />
@@ -41,10 +41,10 @@ const ToggleHomepage = ({ showClient, onToggle }: Props) => {
                 className='text-[12px] md:text-[14px]'
                 style={{ color: themeMode?.darkMode ? 'var(--white)' : 'var(--darkGray)' }}
             >
-                Entities
+                {labelRight}
             </span>
         </div>
     );
 };
 
-export default ToggleHomepage;
+export default Toggle;
