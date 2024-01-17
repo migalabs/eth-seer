3177c9503054433f3bca77eec413bca4abe894d8
Iuri <iuri.19.05@gmail.com>
2024-01-17 18:28:05 +0100

    Add Start and End buttons into Pagination

--- packages/client/components/ui/Pagination.tsx
+++ packages/client/components/ui/Pagination.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
-import { IconButton, Typography } from '@material-tailwind/react';
-import { ArrowRightIcon, ArrowLeftIcon } from '@heroicons/react/24/outline';
+import { Typography } from '@material-tailwind/react';
+import {
+    ArrowLeftIcon,
+    ArrowRightIcon,
+    ChevronDoubleLeftIcon,
+    ChevronDoubleRightIcon,
+} from '@heroicons/react/24/outline';
+
+// Components
+import PaginationIconButton from './PaginationIconButton';
 
 // Props
 type Props = {
@@ -23,18 +31,18 @@ const Pagination = ({ currentPage, totalPages, onChangePage }: Props) => {
     };
 
     return (
-        <div className='flex items-center w-11/12 xl:w-10/12 mx-auto justify-center lg:justify-start gap-10 mt-4 '>
-            <IconButton
-                size='sm'
-                variant='outlined'
-                onClick={prev}
-                disabled={currentPage === 0}
-                className='flex items-center justify-center border-2 border-[var(--black)] dark:border-[var(--white)] text-[var(--black)] dark:text-[var(--white)]'
-            >
-                <ArrowLeftIcon strokeWidth={2} className='h-4 w-4' />
-            </IconButton>
+        <div className='flex items-center w-11/12 xl:w-10/12 mx-auto justify-center lg:justify-start gap-x-4 mt-4 '>
+            <div className='flex gap-x-2'>
+                <PaginationIconButton onClick={() => onChangePage(0)} disabled={currentPage === 0}>
+                    <ChevronDoubleLeftIcon strokeWidth={2} className='h-4 w-4' />
+                </PaginationIconButton>
 
-            <Typography className='font-normal text-[14px] md:text-[16px] text-[var(--black)] dark:text-[var(--white)]'>
+                <PaginationIconButton onClick={prev} disabled={currentPage === 0}>
+                    <ArrowLeftIcon strokeWidth={2} className='h-4 w-4' />
+                </PaginationIconButton>
+            </div>
+
+            <Typography className='font-normal text-[14px] md:text-[16px] text-[var(--black)] dark:text-[var(--white)] text-center'>
                 Page{' '}
                 <strong className='text-[var(--black)] dark:text-[var(--white)]'>
                     {(currentPage + 1).toLocaleString()}
@@ -43,15 +51,18 @@ const Pagination = ({ currentPage, totalPages, onChangePage }: Props) => {
                 <strong className='text-[var(--black)] dark:text-[var(--white)]'>{totalPages.toLocaleString()}</strong>
             </Typography>
 
-            <IconButton
-                size='sm'
-                variant='outlined'
-                onClick={next}
-                disabled={currentPage === totalPages - 1}
-                className='flex items-center justify-center border-2 border-[var(--black)] dark:border-[var(--white)] text-[var(--black)] dark:text-[var(--white)]'
-            >
-                <ArrowRightIcon strokeWidth={2} className='h-4 w-4' />
-            </IconButton>
+            <div className='flex gap-x-2'>
+                <PaginationIconButton onClick={next} disabled={currentPage === totalPages - 1}>
+                    <ArrowRightIcon strokeWidth={2} className='h-4 w-4' />
+                </PaginationIconButton>
+
+                <PaginationIconButton
+                    onClick={() => onChangePage(totalPages - 1)}
+                    disabled={currentPage === totalPages - 1}
+                >
+                    <ChevronDoubleRightIcon strokeWidth={2} className='h-4 w-4' />
+                </PaginationIconButton>
+            </div>
         </div>
     );
 };
