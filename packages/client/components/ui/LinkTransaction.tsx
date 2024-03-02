985074575dc3bee24bd27c97d78ee34fcda6ead0
Iuri <iuri.19.05@gmail.com>
2024-03-02 11:33:18 +0100

    Remove Copy Icon from Transaction Link

--- packages/client/components/ui/LinkTransaction.tsx
+++ packages/client/components/ui/LinkTransaction.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 // Components
 import LinkIcon from './LinkIcon';
 import NetworkLink from './NetworkLink';
-import AddressCopy from './AddressCopy';
+
+// Helpers
+import { getShortAddress } from '../../helpers/addressHelper';
 
 // Props
 type Props = {
@@ -17,13 +19,13 @@ const LinkTransaction = ({ hash, children, mxAuto }: Props) => {
         <NetworkLink
             href={`/transaction/${hash}`}
             passHref
-            className={`flex gap-x-1 items-center text-[14px] md:text-[16px] font-medium md:hover:underline underline-offset-4 decoration-2 w-fit text-[var(--darkPurple)] dark:text-[var(--purple)] ${
+            className={`flex gap-x-1 items-center text-[14px] md:text-[16px] font-medium md:hover:underline underline-offset-4 decoration-2 w-fit text-[var(--darkPurple)] dark:text-[var(--purple)] uppercase ${
                 mxAuto ? 'mx-auto' : ''
             }`}
         >
             {children ?? (
                 <>
-                    <AddressCopy address={hash ?? ''} />
+                    <span>{getShortAddress(hash ?? '')}</span>
                     <LinkIcon />
                 </>
             )}
