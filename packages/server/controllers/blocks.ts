87928866fcb96e494c4cb5953f8cbfad30b2e01e
gerardPolloRebozado <gerardit4@gmail.com>
2025-07-17 09:08:57 +0200

    [fix] sql injection on blocks endpoints

--- packages/server/controllers/blocks.ts
+++ packages/server/controllers/blocks.ts
@@ -1,20 +1,17 @@
-import { Request, Response } from 'express';
-import { clickhouseClients } from '../config/db';
+import { Request, Response } from "express";
+import { clickhouseClients } from "../config/db";
 
 export const getBlocks = async (req: Request, res: Response) => {
+  try {
+    const { network, page = 0, limit = 32 } = req.query;
 
-    try {
+    const chClient = clickhouseClients[network as string];
 
-        const { network, page = 0, limit = 32 } = req.query;
+    const offset = Number(page) * Number(limit);
 
-        const chClient = clickhouseClients[network as string];
-
-        const skip = Number(page) * Number(limit);
-
-        const [blocksResultSet, countResultSet] =
-            await Promise.all([
-                chClient.query({
-                    query: `
+    const [blocksResultSet, countResultSet] = await Promise.all([
+      chClient.query({
+        query: `
                         SELECT
                             f_el_block_number,
                             f_el_block_hash,
@@ -29,49 +26,46 @@ export const getBlocks = async (req: Request, res: Response) => {
                         FROM t_block_metrics
                         WHERE f_el_block_number <> 0
                         ORDER BY f_el_block_number DESC
-                        LIMIT ${Number(limit)}
-                        OFFSET ${skip}
+                        LIMIT {limit: Int64}
+                        OFFSET {offset: Int64}
                     `,
-                    format: 'JSONEachRow',
-                }),
-                chClient.query({
-                    query: `
+        query_params: { limit, offset },
+        format: "JSONEachRow",
+      }),
+      chClient.query({
+        query: `
                         SELECT COUNT(*) AS count
                         FROM t_block_metrics
                         WHERE f_el_block_number <> 0
                     `,
-                    format: 'JSONEachRow',
-                }),
-            ]);
+        format: "JSONEachRow",
+      }),
+    ]);
 
-        const blocksResult = await blocksResultSet.json();
-        const countResult = await countResultSet.json();
+    const blocksResult = await blocksResultSet.json();
+    const countResult = await countResultSet.json();
 
-        res.json({
-            blocks: blocksResult,
-            totalCount: Number(countResult[0].count),
-        }); 
-    
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            msg: 'An error occurred on the server'
-        });
-    }
+    res.json({
+      blocks: blocksResult,
+      totalCount: Number(countResult[0].count),
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: "An error occurred on the server",
+    });
+  }
 };
 
 export const getBlockById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { network } = req.query;
 
-    try {
+    const chClient = clickhouseClients[network as string];
 
-        const { id } = req.params;
-        const { network } = req.query;
-
-        const chClient = clickhouseClients[network as string];
-
-        const blockResultSet =
-            await chClient.query({
-                query: `
+    const blockResultSet = await chClient.query({
+      query: `
                     SELECT
                         f_el_block_number,
                         f_el_block_hash,
@@ -84,37 +78,34 @@ export const getBlockById = async (req: Request, res: Response) => {
                         f_el_transactions,
                         f_payload_size_bytes
                     FROM t_block_metrics
-                    WHERE f_el_block_number = ${id}
+                    WHERE f_el_block_number = {id: Int64}
                     LIMIT 1
                 `,
-                format: 'JSONEachRow',
-            });
+      query_params: { id },
+      format: "JSONEachRow",
+    });
 
-        const blockResult = await blockResultSet.json();
+    const blockResult = await blockResultSet.json();
 
-        res.json({
-            block: blockResult[0],
-        });
-
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            msg: 'An error occurred on the server'
-        });
-    }
+    res.json({
+      block: blockResult[0],
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: "An error occurred on the server",
+    });
+  }
 };
 
 export const getLatestBlock = async (req: Request, res: Response) => {
+  try {
+    const { network } = req.query;
 
-    try {
+    const chClient = clickhouseClients[network as string];
 
-        const { network } = req.query;
-
-        const chClient = clickhouseClients[network as string];
-
-        const blockResultSet =
-            await chClient.query({
-                query: `
+    const blockResultSet = await chClient.query({
+      query: `
                     SELECT
                         f_el_block_number,
                         f_el_block_hash,
@@ -131,35 +122,31 @@ export const getLatestBlock = async (req: Request, res: Response) => {
                     ORDER BY f_el_block_number DESC
                     LIMIT 1
                 `,
-                format: 'JSONEachRow',
-            });
+      format: "JSONEachRow",
+    });
 
-        const blockResult = await blockResultSet.json();
+    const blockResult = await blockResultSet.json();
 
-        res.json({
-            block: blockResult[0],
-        });
-
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            msg: 'An error occurred on the server'
-        });
-    }
+    res.json({
+      block: blockResult[0],
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: "An error occurred on the server",
+    });
+  }
 };
 
 export const getTransactionsByBlock = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { network } = req.query;
 
-    try {
+    const chClient = clickhouseClients[network as string];
 
-        const { id } = req.params;
-        const { network } = req.query;
-
-        const chClient = clickhouseClients[network as string];
-
-        const transactionsResultSet =
-            await chClient.query({
-                query: `
+    const transactionsResultSet = await chClient.query({
+      query: `
                     SELECT
                         f_tx_type,
                         f_value,
@@ -169,21 +156,21 @@ export const getTransactionsByBlock = async (req: Request, res: Response) => {
                         f_timestamp,
                         f_from
                     FROM t_transactions
-                    WHERE f_el_block_number = ${id}
+                    WHERE f_el_block_number = {id: Int64}
                 `,
-                format: 'JSONEachRow',
-            });
+      query_params: { id },
+      format: "JSONEachRow",
+    });
 
-        const transactionsResult = await transactionsResultSet.json();
+    const transactionsResult = await transactionsResultSet.json();
 
-        res.json({
-            transactions: transactionsResult,
-        });
-
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            msg: 'An error occurred on the server'
-        });
-    }
+    res.json({
+      transactions: transactionsResult,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: "An error occurred on the server",
+    });
+  }
 };
